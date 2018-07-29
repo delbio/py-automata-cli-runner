c6d3b72a1f16c0caf76c7e5e8dc758598d0a4303
Fabio Del Bene <delbio87@gmail.com>
2018-07-29 02:06:01 +0200

    improved readability of variables

--- lib/python_helpers.bash
+++ lib/python_helpers.bash
@@ -1,13 +1,13 @@
-python_i=python
-python_pip=pip
+PYTHON_I=python
+PYTHON_PIP=pip
 
 function setPythonVersion() {
     # https://stackoverflow.com/a/677212
     if command -v python3 >/dev/null 2>&1; then
-        python_i=python3
-        python_pip=pip3
+        PYTHON_I=python3
+        PYTHON_PIP=pip3
     else
-        python_i=python
-        python_pip=pip
+        PYTHON_I=python
+        PYTHON_PIP=pip
     fi
 }
