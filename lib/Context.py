c476ba931f02a37648563edda236aaded41ce11a
Fabio Del Bene <delbio87@gmail.com>
2018-11-11 01:51:25 +0100

    added some common mapping parsing phase

--- lib/Context.py
+++ lib/Context.py
@@ -3,6 +3,7 @@ from automaton.builder.XmlBuilder import getClassFromElement, getclass, setPrope
 
 class Context():
     def __init__(self):
+        self.mapping = {}
         pass
 
 
@@ -16,3 +17,23 @@ class XmlContextBuilder():
         handler = Context()
         if root_node is None:
             return handler
+
+        setPropertyOnObject('Property', root_node, handler)
+
+        state_nodes = root_node.findall('States/State')
+        for stateElement in state_nodes:
+            state_name = stateElement.attrib['name']
+            handler.mapping[state_name] = {}
+
+            actionContextHandlerOnStateNodes = stateElement.findall('Actions/Action')
+            for mapElement in actionContextHandlerOnStateNodes:
+                action_name = mapElement.attrib['name']
+                handler.mapping[state_name][action_name] = {}
+
+                # parse context tag as action contex arg
+
+                # parse arg tag as action arg
+
+                # parse result tag as action result mapping to context
+
+        return handler
